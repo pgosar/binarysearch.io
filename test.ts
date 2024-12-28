f6b762977f17b6b2bacaf55110447141d1b117f8
pgosar <pgosarsteam@gmail.com>
2024-12-28 12:54:16 -0600

    Feat: example file to test linting

---
+++ test.ts
@@ -0,0 +1,27 @@
+// Define an enum
+enum Role {
+  Admin = "ADMIN",
+  User = "USER",
+  Guest = "GUEST",
+}
+
+// Interface for a User object
+interface User {
+  name: any;
+  age: any;
+  role: any;
+}
+
+// Class to represent a User
+class UserAccount {
+  name: string;
+  age: number;
+  role: Role;
+
+  constructor(name: string, age: number, role: Role) {
+    this.name = name; this.age = age; this.role = role;
+  }
+
+  greet(): string {return `Hello, my name is ${this.name}, and I am a ${this.role}.`;
+  }
+}
