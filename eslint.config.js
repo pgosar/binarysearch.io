e6354d5359342e06289f50996f62b17ce2e3a50d
Aydan Pirani <aydanpirani@gmail.com>
2024-12-28 22:27:05 -0800

    Another change
    Signed-off-by: Aydan Pirani <aydanpirani@gmail.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
-{
+module.exports = {
   // Configuration for JavaScript files
   "extends": [
     "next/core-web-vitals", // Needed to avoid warning in next.js build: 'The Next.js plugin was not detected in your ESLint configuration'
