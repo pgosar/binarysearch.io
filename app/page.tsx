7829b5f9a5e83d780c03af4c7fe0bcb2af4e28d4
Omer Ahmed Khan <oxk4769@mavs.uta.edu>
2024-12-28 22:44:29 -0600

    Testing "yarn format" for CI/CD v2

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Header from './client/components/Header';
 const HomePage = () => {
   return (
     <div>
-      <Header /><h2>Should be fixed by prettier!</h2></div>
+      <Header /><h2>Pls work!</h2></div>
   );
 };
 
