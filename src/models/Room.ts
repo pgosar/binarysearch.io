c59733460e56cb5c3802ebdb60035d6c4ce67aa5
Aydan Pirani <aydanpirani@gmail.com>
2025-01-01 17:35:16 -0800

    Fixed linter

--- src/models/Room.ts
+++ src/models/Room.ts
@@ -7,6 +7,7 @@ const ROOM_CODE_LENGTH = 6;
 
 export const zRoomData = z.object({
   tags: z.array(z.string()),
+  isPublic: z.boolean(),
 });
 
 export const zRoomId = z.object({
@@ -16,8 +17,9 @@ export const zRoomId = z.object({
 // Minimum to create a room
 export const RoomSchema = new Schema({
   roomId: { type: String, required: false, unique: true, default: () => v4() },
-  participants: { type: [String], required: false, default: () => [] },
   code: { type: String, required: false, unique: true, default: () => generateRandomString(ROOM_CODE_LENGTH) },
+  participants: { type: [String], required: false, default: () => [] },
+  isPublic: { type: Boolean, required: true },
   tags: { type: String, required: false },
   leader: { type: String, required: true },
 });
